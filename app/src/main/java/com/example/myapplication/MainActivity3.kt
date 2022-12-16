af5524ee419dc0d6975f7c9eeb64bd9c4ef3ec42
yuanrongheng <2177617128@qq.com>
2022-12-16 11:42:59 +0800

    周五

--- app/src/main/java/com/example/myapplication/MainActivity3.kt
+++ app/src/main/java/com/example/myapplication/MainActivity3.kt
@@ -2,10 +2,15 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 
 class MainActivity3 : AppCompatActivity() {
+    private lateinit var tu : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
+        tu=findViewById(R.id.tupian)
+        Glide.with(this).load("https://qulian-prod.oss-cn-hangzhou.aliyuncs.com/room/bubble/kuaile2.png").into(tu);
     }
 }
